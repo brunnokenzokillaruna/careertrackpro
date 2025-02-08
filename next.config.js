51203b2742bf81b41012ff87112df1185f90ceb5
Brunno Kenzo <brunnokenzokillaruna@gmail.com>
2025-02-08 16:07:53 -0500

    Fix font and CSS loading issues

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
-const repoName = 'CareerTrack-Pro';
+const repoName = 'careertrackpro';
 
 const nextConfig = {
   // This setting tells Next.js to produce a static build.
@@ -9,18 +9,8 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: isProduction ? `/${repoName}` : '',
-  assetPrefix: isProduction ? `/${repoName}/` : '',
+  assetPrefix: isProduction ? `/${repoName}` : '',
   trailingSlash: true,
-  // Add webpack configuration to handle CSS properly
-  webpack: (config) => {
-    if (isProduction) {
-      config.output = {
-        ...config.output,
-        publicPath: `/${repoName}/`,
-      };
-    }
-    return config;
-  },
 };
 
 module.exports = nextConfig; 
